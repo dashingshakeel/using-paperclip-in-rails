2f6a88963904df15e62024b6a30237f6e88cc98c
dashingshakeel <shkeelrauf@gmail.com>
2017-02-02 15:51:55 +0500

    Addition changes for error removing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_attached_file :avatar,styles: {medium: "300x300",thumb: "100x100"}
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  do_not_validate_attachment_file_type :avatar
 end
